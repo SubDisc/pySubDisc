1857f7ce948f1e8029552e0aae0d603a10f66d0d
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2023-08-23 18:16:36 +0200

    Update some defaults to match GUI

--- src/pysubdisc/core.py
+++ src/pysubdisc/core.py
@@ -27,7 +27,7 @@ class SubgroupDiscovery(object):
   def targetType(self):
     return self._targetConcept.getTargetType().toString()
 
-  def _initSearchParameters(self, *, qualityMeasure='cortana quality', qualityMeasureMinimum=0.1, searchDepth=1, minimumCoverage=2, maximumCoverageFraction=1.0, minimumSupport=0, maximumSubgroups=1000, filterSubgroups=True, minimumImprovement=0.0, maximumTime=1000, searchStrategy='beam', nominalSets=False, numericOperatorSetting='normal', numericStrategy='best', searchStrategyWidth=10, nrBins=8, nrThreads=1):
+  def _initSearchParameters(self, *, qualityMeasure='cortana quality', qualityMeasureMinimum=0.1, searchDepth=1, minimumCoverage=2, maximumCoverageFraction=0.9, minimumSupport=0, maximumSubgroups=1000, filterSubgroups=True, minimumImprovement=0.0, maximumTime=0, searchStrategy='beam', nominalSets=False, numericOperatorSetting='normal', numericStrategy='bins', searchStrategyWidth=10, nrBins=8, nrThreads=1):
     # use inspect to avoid duplicating the argument list
     from inspect import signature
     sig = signature(self._initSearchParameters)

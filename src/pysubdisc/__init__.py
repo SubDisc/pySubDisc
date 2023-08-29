9a98a8685863164a39d92e55a05efbcf7b268eaf
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2023-08-29 11:28:20 +0200

    Use single source for package version

--- src/pysubdisc/__init__.py
+++ src/pysubdisc/__init__.py
@@ -1,6 +1,10 @@
-__version__="0.0.1"
-
 from .interface import singleNominalTarget, singleNumericTarget
 
 __all__ = [ singleNominalTarget, singleNumericTarget ]
 
+
+# Set package version
+from os import path
+curdir = path.abspath(path.dirname(__file__))
+with open(path.join(curdir, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()

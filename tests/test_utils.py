4e195eaa2f323d9086d45bbdd9d61aa7ba660b7b
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2023-08-31 11:16:02 +0200

    Add more getModel tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,9 +17,14 @@ def test_compute_threshold(adult_data):
   assert t == pytest.approx(0.1004, abs=0.001)
 
 
-def test_getmodel(adult_data):
+def test_getmodel_singlenumeric(adult_data):
   sd = pysubdisc.singleNumericTarget(adult_data, 'age')
+  df = sd.getBaseModel(verbose=False)
+  assert df['base'].mean() == pytest.approx(0.010283, abs=1e-6)
+  assert df['base'].std() == pytest.approx(0.0104838, abs=1e-6)
+
   sd.run(verbose=False)
+
   df = sd.getModel(0, verbose=False)
   assert df['base'].mean() == pytest.approx(0.010283, abs=1e-6)
   assert df['base'].std() == pytest.approx(0.0104838, abs=1e-6)
@@ -33,3 +38,35 @@ def test_getmodel(adult_data):
   assert df[0].std() == pytest.approx(0.004420, abs=1e-6)
   assert df[1].mean() == pytest.approx(0.004555, abs=1e-6)
   assert df[1].std() == pytest.approx(0.005234, abs=1e-6)
+
+  df = sd.getModel(0, includeBase=False, verbose=False)
+  with pytest.raises(KeyError):
+    _ = df['base']
+  assert df[0].mean() == pytest.approx(0.003866, abs=1e-6)
+  assert df[0].std() == pytest.approx(0.004420, abs=1e-6)
+
+
+def test_getmodel_doubleregression(adult_data):
+  sd = pysubdisc.doubleRegressionTarget(adult_data, 'age', 'hours-per-week')
+  df = sd.getBaseModel(verbose=False)
+  assert all(df.columns == ['x', 'y base', 'pred base'])
+  assert all(df.index == adult_data.index)
+
+  sd.run(verbose=False)
+
+  df = sd.asDataFrame()
+  assert df['Slope'][0] == pytest.approx(0.345898, abs=1e-6)
+  assert df['Intercept'][0] == pytest.approx(26.339434, abs=1e-6)
+
+  df = sd.getModel(0, verbose=False)
+  assert all(df.columns == ['x', 'y base', 'pred base', 'y 0', 'pred 0'])
+  assert df['y base'].count() == 1000
+  assert df['y 0'].count() == 344
+  assert df['pred 0'].count() == 1000
+  assert df['pred 0'][0] ==  pytest.approx(39.0 * 0.345898 + 26.339434, abs=1e-4)
+
+  df = sd.getModel(0, includeBase=False, verbose=False)
+  assert all(df.columns == ['x', 'y 0', 'pred 0'])
+  assert df['y 0'].count() == 344
+  assert df['pred 0'].count() == 1000
+  assert df['pred 0'][0] ==  pytest.approx(39.0 * 0.345898 + 26.339434, abs=1e-4)

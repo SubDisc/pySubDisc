9e73212f92fefdd96dfdc88ab1efbdced2181dc9
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2023-09-14 23:18:01 +0200

    Move version parsing to version.py
    This cleans up the top level module

--- src/pysubdisc/__init__.py
+++ src/pysubdisc/__init__.py
@@ -1,3 +1,5 @@
+from .version import __version__
+
 from .interface import singleNominalTarget
 from .interface import singleNumericTarget
 from .interface import doubleRegressionTarget
@@ -13,11 +15,3 @@ __all__ = [ singleNominalTarget,
             doubleCorrelationTarget,
             multiNumericTarget,
             loadDataFrame ]
-
-
-# Set package version
-from os import path
-from re import match
-curdir = path.abspath(path.dirname(__file__))
-with open(path.join(curdir, 'VERSION')) as version_file:
-    __version__ = match("^pySubDisc (?P<version>.+)", version_file.read().strip()).group('version')
